b1d96e23cd1609da3a8be0bb4bbedb28bde07900
mudassar <videosblast@gmail.com>
2020-10-15 13:06:06 +0500

    Nav

---
+++ main.js
@@ -0,0 +1,11 @@
+// Selecting ham menu
+
+let menu = document.querySelector(".ham-menu");
+
+menu.addEventListener("click", (e) => {
+  // selecting the nav menu and adding respo call to it
+  let navMenu = document.querySelector(".nav-menu");
+  navMenu.classList.toggle("respo");
+
+  //   animation the ham menu icon
+});
